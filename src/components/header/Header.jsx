f26f1b13b23746dd93878293c62a053e8c08eaed
Ayush <ayush.yadav.100100@gmail.com>
2024-03-12 06:24:01 +0530

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { Drawer } from '@mui/material'
+import './header.scss'
+import { Close, LocationOn } from '@mui/icons-material'
+
+function Header() {
+  const [open, setOpen] = useState(false)
+  return (
+    <div className="headerContainer">
+      <img src="/images/hamburger.png" onClick={() => setOpen(true)} />
+      <img src="/images/search.png" />
+      <Drawer anchor={'left'} open={open}>
+        <div className="sideBarWrapper">
+          <div className="header">
+            <Close fontSize="large" onClick={() => setOpen(false)} />
+            <span>Live</span>
+          </div>
+          <div className="sidebarContent">
+            <div>
+              <LocationOn />
+              <p>CURRENT LOCATION</p>
+            </div>
+            <h2>
+              New York, <br /> United States
+            </h2>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+  )
+}
+
+export default Header
